73b5a8e840bc63467783d008638db36bf6c97fb4
lenicliu <lenicliu@163.com>
2016-06-22 20:40:41 +0800

    update

--- eg-java-thread/src/main/java/com/lenicliu/thread/Main20SameTime.java
+++ eg-java-thread/src/main/java/com/lenicliu/thread/Main20SameTime.java
@@ -1,30 +1,102 @@
 package com.lenicliu.thread;
 
-public class Main20 {
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+public class Main20SameTime {
+	private static final int n = 6;
 
 	public static void main(String[] args) throws InterruptedException {
-		Object o = new Object();
-		Runnable r = () -> {
+		start(n, new ObjectGate());
+		start(n, new CountLatchGate());
+		start(n, new CyclicBarrierGate(n));
+	}
+
+	static interface Gate {
+		void ready();
+
+		void go();
+	}
+
+	static class ObjectGate implements Gate {
+		private final Object o = new Object();
+
+		@Override
+		public void ready() {
 			synchronized (o) {
 				try {
 					o.wait();
 				} catch (InterruptedException e) {
-					e.printStackTrace();
 				}
 			}
-			System.out.println(System.currentTimeMillis());
-		};
-
-		new Thread(r).start();
-		new Thread(r).start();
-		new Thread(r).start();
-		new Thread(r).start();
-		new Thread(r).start();
-		new Thread(r).start();
-
-		Thread.sleep(100);
-		synchronized (o) {
-			o.notifyAll();
 		}
+
+		@Override
+		public void go() {
+			synchronized (o) {
+				o.notifyAll();
+			}
+		}
+	}
+
+	static class CountLatchGate implements Gate {
+		private final CountDownLatch latch = new CountDownLatch(1);
+
+		@Override
+		public void ready() {
+			try {
+				latch.await();
+			} catch (InterruptedException e) {
+			}
+		}
+
+		@Override
+		public void go() {
+			latch.countDown();
+		}
+	}
+
+	static class CyclicBarrierGate implements Gate {
+		private final CyclicBarrier barrier;
+
+		CyclicBarrierGate(int n) {
+			barrier = new CyclicBarrier(n);
+		}
+
+		@Override
+		public void ready() {
+			try {
+				barrier.await();
+			} catch (InterruptedException e) {
+			} catch (BrokenBarrierException e) {
+			}
+		}
+
+		@Override
+		public void go() {
+			// do nothing
+		}
+	}
+
+	private static void start(final int n, final Gate gate) throws InterruptedException {
+		for (int i = 0; i < n; i++) {
+			new Thread(() -> {
+				try {
+					gate.ready();
+				} catch (Exception e) {
+				}
+				System.out.println(fix(gate.getClass().getSimpleName()) + "\t" + System.currentTimeMillis());
+			}).start();
+		}
+		Thread.sleep(100);
+
+		gate.go();
+		Thread.sleep(100);
+		System.out.println();
+	}
+
+	private static String fix(String value) {
+		return value == null || value.length() < 10 ? value : value.substring(0, 10) + "...";
 	}
 }
